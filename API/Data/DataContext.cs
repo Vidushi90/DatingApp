ccd4f87bf5a1ec61f2944371a56fba922fe94a94
vidushi <vidu.bisen@gmail.com>
2024-03-04 12:35:15 +0530

    First Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using API.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
