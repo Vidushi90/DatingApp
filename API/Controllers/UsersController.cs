6356e7eebcb5df20fad20b18e0c6f5d4241123fc
vidushi <vidu.bisen@gmail.com>
2024-03-18 01:55:02 +0530

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,15 @@
-﻿using API.Data;
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
 using API.Entites;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -15,6 +17,7 @@ public class UsersController : ControllerBase
         this._context = context;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
         var users = await this._context.Users.ToListAsync();
@@ -24,6 +27,24 @@ public class UsersController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id) {
         return await this._context.Users.FindAsync(id);
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(string userName, string password) {
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser {
+            UserName = userName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+
     }
 
 }
